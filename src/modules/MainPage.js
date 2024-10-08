2b7a7c630e1f4a5284bf5e171593206cffbd8ca5
LongCT_ <thanhlong2704031@gmail.com>
2024-10-08 13:57:28 +0700

    added Helmet to MainPage

--- src/modules/MainPage.js
+++ src/modules/MainPage.js
@@ -4,6 +4,9 @@ import { Helmet } from "react-helmet-async";
 const MainPage = () => {
   return (
     <div className="mx-2">
+      <Helmet>
+        <title>LongCT_'s Toolbox</title>
+      </Helmet>
       <p className="text-lg mb-4">
         Hello, welcome to my toolbox. I store all of my tools here.
       </p>
